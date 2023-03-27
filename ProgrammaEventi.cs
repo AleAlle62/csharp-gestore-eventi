80ebbeb9f6dbb60f7e16b29c953e382ada4b4bdd
AleAlle62 <basketallegrini@gmail.com>
2023-03-27 15:28:01 +0200

    M2

--- ProgrammaEventi.cs
+++ ProgrammaEventi.cs
@@ -8,6 +8,12 @@ namespace csharp_gestore_eventi
 {
     public class ProgrammaEventi
     {
-        public string Titolo 
+        public string Titolo { get; set; }
+        public List<Evento> Eventi { get; set; }
+        public ProgrammaEventi(string Titolo)
+        {
+            Titolo = Titolo;
+            Eventi = new List<Evento>();
+        }
     }
 }
