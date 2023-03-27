3bd731bb027c35400e64d2caed8063d279da1479
AleAlle62 <basketallegrini@gmail.com>
2023-03-27 15:07:42 +0200

    M2

--- Evento.cs
+++ Evento.cs
@@ -9,16 +9,16 @@ namespace csharp_gestore_eventi
     public class Evento 
     {
         //attributi 
-        private string _titolo;
-        private DateTime _data;
-        private int _capienzaMassima;
-        private int _numeroPostiPrenotati;
+        public string _titolo;
+        public DateTime _data;
+        public int _capienzaMassima;
+        public int _numeroPostiPrenotati;
 
-        public Evento(string titolo, DateTime data, int CapienzaMassima)
+        public Evento(string titolo, DateTime data, int capienzaMassima)
         {
             Titolo = titolo;
             Data = data;
-            CapienzaMassima = CapienzaMassima;
+            _capienzaMassima = capienzaMassima;
             _numeroPostiPrenotati = 0;
         }
 
@@ -30,7 +30,7 @@ namespace csharp_gestore_eventi
             {
                 //controllo che il titolo non sia vuoto 
                 if (string.IsNullOrEmpty(value))
-                    throw new ArgumentException("Il titolo non può essere vuoto.");
+                    throw new Exception("Il titolo non può essere vuoto.");
                 _titolo = value;
             }
         }
@@ -43,20 +43,20 @@ namespace csharp_gestore_eventi
             {
                 // controllo che la data non sia nel passato
                 if (value < DateTime.Now)
-                    throw new ArgumentException("La data non può essere nel passato.");
+                    throw new Exception("La data non può essere nel passato.");
                 _data = value;
             }
         }
 
         // 3) capienza massima
-        public int CapienzaMassima
+        public int capienzaMassima
         {
             get { return _capienzaMassima; }
             set
             {
                 // controllo che non sia un numero sotto lo 0
                 if (value <= 0)
-                    throw new ArgumentException("La capienza massima deve essere un numero positivo.");
+                    throw new Exception("La capienza massima deve essere un numero positivo.");
                 _capienzaMassima = value;
             }
         }
@@ -77,11 +77,11 @@ namespace csharp_gestore_eventi
         public bool PrenotaPosti(int postiDaPrenotare)
         {
             if (Data < DateTime.Now)
-                throw new InvalidOperationException("L'evento è già passato.");
-            if (_numeroPostiPrenotati >= _capienzaMassima)
-                throw new InvalidOperationException("L'evento ha raggiunto la capienza massima.");
+                throw new Exception("L'evento è già passato.");
+            if (_numeroPostiPrenotati > _capienzaMassima)
+                throw new Exception("L'evento ha raggiunto la capienza massima.");
             if (_numeroPostiPrenotati + postiDaPrenotare > _capienzaMassima)
-                throw new InvalidOperationException("Non ci sono abbastanza posti disponibili per l'evento.");
+                throw new Exception("Non ci sono abbastanza posti disponibili per l'evento.");
             _numeroPostiPrenotati += postiDaPrenotare;
             return true;
         }
@@ -91,11 +91,11 @@ namespace csharp_gestore_eventi
         public bool DisdiciPosti(int postiDaDisdire)
         {
             if (Data < DateTime.Now)
-                throw new InvalidOperationException("L'evento è già passato.");
+                throw new Exception("L'evento è già passato.");
             if (_numeroPostiPrenotati <= 0)
-                throw new InvalidOperationException("Non ci sono posti prenotati per l'evento.");
+                throw new Exception("Non ci sono posti prenotati per l'evento.");
             if (_numeroPostiPrenotati - postiDaDisdire < 0)
-                throw new InvalidOperationException("Non ci sono abbastanza posti prenotati da disdire.");
+                throw new Exception("Non ci sono abbastanza posti prenotati da disdire.");
             _numeroPostiPrenotati -= postiDaDisdire;
             return true;
         }
@@ -106,3 +106,4 @@ namespace csharp_gestore_eventi
         }
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -1,1 +1,102 @@
-﻿
+﻿using csharp_gestore_eventi;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Chiedi all'utente di inserire i dati per un nuovo evento
+        Console.WriteLine("Inserisci i dati per un nuovo evento:");
+        Console.Write("Titolo: ");
+        string titolo = Console.ReadLine();
+        Console.Write("Data (formato dd/MM/yyyy): ");
+        string dataStringa = Console.ReadLine();
+        DateTime data;
+        if (!DateTime.TryParseExact(dataStringa, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out data))
+        {
+            Console.WriteLine("Data non valida.");
+            return;
+        }
+        Console.Write("Capienza massima: ");
+        int capienzaMassima;
+        if (!int.TryParse(Console.ReadLine(), out capienzaMassima))
+        {
+            Console.WriteLine("Capienza massima non valida.");
+            return;
+        }
+
+        // Crea un nuovo evento con i dati inseriti dall'utente
+        Evento evento = new Evento(titolo, data, capienzaMassima);
+
+        // Chiedi all'utente di effettuare delle prenotazioni
+        while (true)
+        {
+            Console.Write("Vuoi prenotare dei posti? (s/n): ");
+            string risposta = Console.ReadLine();
+            if (risposta.ToLower() == "n")
+                break;
+            Console.Write("Quanti posti vuoi prenotare? ");
+            int postiDaPrenotare;
+            if (!int.TryParse(Console.ReadLine(), out postiDaPrenotare))
+            {
+                Console.WriteLine("Numero di posti non valido.");
+                continue;
+            }
+            if (evento.PrenotaPosti(postiDaPrenotare))
+            {
+                Console.WriteLine("Prenotazione effettuata.");
+                Console.WriteLine("Posti prenotati: " + evento.NumeroPostiPrenotati);
+                Console.WriteLine("Posti disponibili: " + (evento.capienzaMassima - evento.NumeroPostiPrenotati));
+            }
+            else
+            {
+                Console.WriteLine("Prenotazione non effettuata.");
+            }
+        }
+
+
+        // Chiedi all'utente di disdire dei posti
+        while (true)
+        {
+            Console.Write("Vuoi disdire dei posti? (s/n): ");
+            string risposta = Console.ReadLine();
+            if (risposta.ToLower() == "n")
+                break;
+            Console.Write("Quanti posti vuoi disdire? ");
+            int postiDaDisdire;
+            if (!int.TryParse(Console.ReadLine(), out postiDaDisdire))
+            {
+                Console.WriteLine("Numero di posti non valido.");
+                continue;
+            }
+            if (evento.DisdiciPosti(postiDaDisdire))
+            {
+                Console.WriteLine("Disdetta effettuata.");
+                Console.WriteLine("Posti prenotati: " + evento.NumeroPostiPrenotati);
+                Console.WriteLine("Posti disponibili: " + (evento.capienzaMassima - evento.NumeroPostiPrenotati));
+            }
+            else
+            {
+                Console.WriteLine("Disdetta non effettuata.");
+            }
+        }
+
+
+        //stampa dei dati inseriti
+        Console.WriteLine("");
+        Console.WriteLine("Titolo:" + evento.Titolo);
+       
+        Console.WriteLine("");
+        Console.WriteLine("Data:" + evento.Data.ToString("dd/MM/yyyy"));
+
+        Console.WriteLine("");
+        Console.WriteLine("Capienza massima:" + evento.capienzaMassima);
+
+        Console.WriteLine("");
+        Console.WriteLine("POsti prenotati:" + evento.NumeroPostiPrenotati);
+
+        Console.ReadLine();
+    }
+}
+
+
